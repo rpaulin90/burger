354bfcb94ee0889dcb3bed365a0284e5bf212d41
Ricardo Paulin <rpaulin@ricardos-mbp.attlocal.net>
2017-06-11 13:18:06 -0500

    added JAWS_DB connection

--- config/connection.js
+++ config/connection.js
@@ -5,21 +5,38 @@
 var mysql = require("mysql");
 
 // create the connection information for the sql database
-var connection = mysql.createConnection({
-    host: "localhost",
-
-    // Your username
-    user: "root",
-
-    // Your password
-    password: "",
-    database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//
+//     // Your username
+//     user: "root",
+//
+//     // Your password
+//     password: "",
+//     database: "burgers_db"
+// });
 
 // connect to the mysql server and sql database
 // connection.connect(function(err) {
 //     if (err) throw err;
 // });
+
+if (process.env.JAWSDB_URL) {
+
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+    connection = mysql.createConnection({
+        host: "localhost",
+
+        // Your username
+        user: "root",
+
+        // Your password
+        password: "",
+        database: "burgers_db"
+    });
+}
 
 connection.connect(function(err) {
     if (err) throw err;
